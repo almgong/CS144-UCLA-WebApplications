76e9a72d1cf0e78733d3b34dfcfbe1501851f0ba
Mark Ketenjian <maketenjian@gmail.com>
2016-02-06 21:55:03 -0800

    Initial p3 commit. build and drop scripts for spatial index. Added Mark's UID to team.txt.

---
+++ p3/indexer/buildSQLIndex.sql
@@ -0,0 +1,3 @@
+CREATE TABLE geom (item_id int PRIMARY KEY, g GEOMETRY NOT NULL, SPATIAL INDEX(g)) ENGINE=MyISAM;
+
+INSERT INTO geom select item_id, Point(latitude, longitude) from Item where latitude != 0 and longitude != 0;
